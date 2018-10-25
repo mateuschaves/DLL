3846dd8ed108a5d2b14d19b3883c50314f1a38af
Flavio Morais <33457808+161996@users.noreply.github.com>
2018-10-25 16:18:51 -0300

    Update dll.h

--- dll.h
+++ dll.h
@@ -6,3 +6,8 @@
  
 MINHADLL void soma_vetores(int vec1[], int vec2[], int r[], int rn);
 MINHADLL void sub_vetores(int vec1[], int vec2[], int r[], int rn);
+MINHADLL void mult_vetores(int vec1[], int vec2[], int r[], int rn);
+MINHADLL void div_vetores(int vec1[], int vec2[], int r[], int rn);
+MINHADLL int produto_Escalar(int vec1[], int vec2[], int rn);
+MINHADLL int produto_vetorial(int vec1[], int vec2[], int vet[]);
+MINHADLL int produto_Misto(int vec1[], int vec2[], int vec3[], int vet[], int r);
