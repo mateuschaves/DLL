f76a9acf2da7ba89f33b96b0f5485fe9d54245e1
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-10-27 16:32:41 -0300

    Tradução das funções

--- dll.h
+++ dll.h
@@ -4,15 +4,15 @@
  #define analytical_geometry __declspec(dllimport)
 #endif
  
-analytical_geometry void soma_vetores(int vec1[], int vec2[], int r[], int rn);
-analytical_geometry void sub_vetores(int vec1[], int vec2[], int r[], int rn);
-analytical_geometry void mult_vetores(int vec1[], int vec2[], int r[], int rn);
-analytical_geometry void div_vetores(int vec1[], int vec2[], int r[], int rn);
-analytical_geometry int produto_Escalar(int vec1[], int vec2[], int rn);
-analytical_geometry void produto_vetorial(int vec1[], int vec2[], int vet[]);
-analytical_geometry int produto_Misto(int vec1[], int vec2[], int vec3[], int vet[], int r);
-analytical_geometry float modulo_vetor(int vec[], int rn, int r, float rs);
-analytical_geometry void projecao(int vec1[], int vec2[], float r[], int rn);
-analytical_geometry float angulo(int vec1[], int vec2[], int rn);
-analytical_geometry float radianos_to_grau(float rad);
-analytical_geometry void angulos_diretores(int vec[], float r[], int rn);
+analytical_geometry void vector_sum(int vec1[], int vec2[], int r[], int rn);
+analytical_geometry void vector_subtraction(int vec1[], int vec2[], int r[], int rn);
+analytical_geometry void vector_multiplication(int vec1[], int vec2[], int r[], int rn);
+analytical_geometry void vector_division(int vec1[], int vec2[], int r[], int rn);
+analytical_geometry int scalar_product(int vec1[], int vec2[], int rn);
+analytical_geometry void vector_product(int vec1[], int vec2[], int vet[]);
+analytical_geometry int mixed_product(int vec1[], int vec2[], int vec3[], int vet[], int r);
+analytical_geometry float module_vector(int vec[], int rn, int r, float rs);
+analytical_geometry void orthogonal_projection(int vec1[], int vec2[], float r[], int rn);
+analytical_geometry float angle_between_vectors(int vec1[], int vec2[], int rn);
+analytical_geometry float radians_to_degrees(float rad);
+analytical_geometry void angles_directors(int vec[], float r[], int rn);
