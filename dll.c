256ea13f5ea0c38ec5c073243b42bb4bc29e5883
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-10-27 16:23:16 -0300

    Mudando o nome da DLL

--- dll.c
+++ dll.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 #include "dll.h"
 #include <math.h>
- 
-MINHADLL void soma_vetores(int vec1[], int vec2[], int r[], int rn)
+
+analytical_geometry void soma_vetores(int vec1[], int vec2[], int r[], int rn)
 {
     for(int i = 0; i < rn; i++)
         r[i] = vec1[i] + vec2[i];
 }
 
-MINHADLL void sub_vetores(int vec1[], int vec2[], int r[], int rn)
+analytical_geometry void sub_vetores(int vec1[], int vec2[], int r[], int rn)
 {
     for(int i = 0; i < rn; i++)
         r[i] = vec1[i] - vec2[i];
 }
 
-MINHADLL void mult_vetores(int vec1[], int vec2[], int r[], int rn)
+analytical_geometry void mult_vetores(int vec1[], int vec2[], int r[], int rn)
 {
     for(int i = 0; i < rn; i++)
         r[i] = vec1[i] * vec2[i];
 }
-MINHADLL void div_vetores(int vec1[], int vec2[], int r[], int rn)
+analytical_geometry void div_vetores(int vec1[], int vec2[], int r[], int rn)
 {
     for(int i = 0; i < rn; i++)
         r[i] = vec1[i] / vec2[i];
 }
 
 
-MINHADLL int produto_Escalar(int vec1[], int vec2[],int rn){
+analytical_geometry int produto_Escalar(int vec1[], int vec2[],int rn){
    int r;
    for (int i=0; i<rn; i++){ 
     r += vec1[i] * vec2[i];
@@ -34,13 +34,13 @@ MINHADLL int produto_Escalar(int vec1[], int vec2[],int rn){
       return r;
 }
 
-MINHADLL void produto_vetorial(int vec1[], int vec2[], int vet[]){
+analytical_geometry void produto_vetorial(int vec1[], int vec2[], int vet[]){
     vet[0] =  ((vec1[1]*vec2[2]) - (vec1[2]*vec2[1]));
     vet[1] =  - (((vec1[0]*vec2[2]) - (vec1[2]*vec2[0]))); 
     vet[2] =  ((vec1[0]*vec2[1]) - (vec1[1]*vec2[0]));
 }
 
-MINHADLL int produto_Misto(int vec1[], int vec2[], int vec3[], int vet[], int r){
+analytical_geometry int produto_Misto(int vec1[], int vec2[], int vec3[], int vet[], int r){
    int i;
     r=0;
    for (i=0;i<3;i++){ 
@@ -53,7 +53,7 @@ r +=vet[i]*vec3[i];
 }
 return r;
 }
-MINHADLL float modulo_vetor(int vec[], int rn, int r, float rs){
+analytical_geometry float modulo_vetor(int vec[], int rn, int r, float rs){
     for(int i = 0; i < rn; i++){ 
         r += (vec[i] * vec[i]);
         rs = sqrt(r);
@@ -61,7 +61,7 @@ MINHADLL float modulo_vetor(int vec[], int rn, int r, float rs){
     return rs;
 }
 
-MINHADLL void projecao(int vec1[], int vec2[], float r[], int rn){
+analytical_geometry void projecao(int vec1[], int vec2[], float r[], int rn){
     int pe = produto_Escalar(vec1, vec2, rn);
     int modulo = 0;
     for(int i = 0; i < rn; i++){
@@ -73,15 +73,15 @@ MINHADLL void projecao(int vec1[], int vec2[], float r[], int rn){
     }
 }
 
-MINHADLL float angulo(int vec1[], int vec2[], int rn){
+analytical_geometry float angulo(int vec1[], int vec2[], int rn){
     return radianos_to_grau(acos(produto_Escalar(vec1, vec2, rn) / (modulo_vetor(vec1, rn, 0, 0) * modulo_vetor(vec2, rn, 0, 0))));
 }
 
-MINHADLL float radianos_to_grau(float rad){
+analytical_geometry float radianos_to_grau(float rad){
     return rad * 57.2958;
 }
 
-MINHADLL void angulos_diretores(int vec[], float r[], int rn){
+analytical_geometry void angulos_diretores(int vec[], float r[], int rn){
     float mod = modulo_vetor(vec, rn, 0, 0);
     for(int i =0; i < rn; i++){
         r[i] = radianos_to_grau(acos(vec[i] / mod));
--- dll.h
+++ dll.h
@@ -1,18 +1,18 @@
 #ifdef BUILD_DLL
- #define MINHADLL __declspec(dllexport)
+ #define analytical_geometry  __declspec(dllexport)
 #else
- #define MINHADLL __declspec(dllimport)
+ #define analytical_geometry __declspec(dllimport)
 #endif
  
-MINHADLL void soma_vetores(int vec1[], int vec2[], int r[], int rn);
-MINHADLL void sub_vetores(int vec1[], int vec2[], int r[], int rn);
-MINHADLL void mult_vetores(int vec1[], int vec2[], int r[], int rn);
-MINHADLL void div_vetores(int vec1[], int vec2[], int r[], int rn);
-MINHADLL int produto_Escalar(int vec1[], int vec2[], int rn);
-MINHADLL void produto_vetorial(int vec1[], int vec2[], int vet[]);
-MINHADLL int produto_Misto(int vec1[], int vec2[], int vec3[], int vet[], int r);
-MINHADLL float modulo_vetor(int vec[], int rn, int r, float rs);
-MINHADLL void projecao(int vec1[], int vec2[], float r[], int rn);
-MINHADLL float angulo(int vec1[], int vec2[], int rn);
-MINHADLL float radianos_to_grau(float rad);
-MINHADLL void angulos_diretores(int vec[], float r[], int rn);
+analytical_geometry void soma_vetores(int vec1[], int vec2[], int r[], int rn);
+analytical_geometry void sub_vetores(int vec1[], int vec2[], int r[], int rn);
+analytical_geometry void mult_vetores(int vec1[], int vec2[], int r[], int rn);
+analytical_geometry void div_vetores(int vec1[], int vec2[], int r[], int rn);
+analytical_geometry int produto_Escalar(int vec1[], int vec2[], int rn);
+analytical_geometry void produto_vetorial(int vec1[], int vec2[], int vet[]);
+analytical_geometry int produto_Misto(int vec1[], int vec2[], int vec3[], int vet[], int r);
+analytical_geometry float modulo_vetor(int vec[], int rn, int r, float rs);
+analytical_geometry void projecao(int vec1[], int vec2[], float r[], int rn);
+analytical_geometry float angulo(int vec1[], int vec2[], int rn);
+analytical_geometry float radianos_to_grau(float rad);
+analytical_geometry void angulos_diretores(int vec[], float r[], int rn);
