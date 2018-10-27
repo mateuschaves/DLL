a6de81232c4d55865eda45bf3f53233e252370ea
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-10-27 19:18:58 -0300

    Modificando o exemplo

--- exe.c
+++ exe.c
@@ -7,7 +7,7 @@ int main(int argc,char **argv)
 {
  int vec1[3] = {3, -4, 12};
  float r[3];
- angulos_diretores(vec1, r, 3);
+ angles_directors(vec1, r, 3);
  for(int i = 0 ; i < 3; i++)
     printf("%0.2f\n", r[i]);
  system("pause");
